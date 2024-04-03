48c99171ef94633b9ab58ccfe1f6b372018ffcd1
weimeilin79 <weimeilin@gmail.com>
2024-04-03 17:48:44 -0400

    vercel

---
+++ cupcake-pusher/api/submit.js
@@ -0,0 +1,29 @@
+const { Kafka } = require("kafkajs");
+
+const redpanda = new Kafka({
+  clientId: 'store-app',
+  brokers: [process.env.SERVERLESSBROKER],
+  ssl: {},
+  sasl: {
+      mechanism: "scram-sha-256",
+      username: process.env.RPUSER,
+      password: process.env.RPPWD
+  }
+});
+
+const producer = redpanda.producer();
+
+module.exports = async (req, res) => {
+    const { store, blueberry, strawberry } = req.body;
+
+    await producer.connect();
+    await producer.send({
+        topic: 'inv-count',
+        messages: [
+            { value: JSON.stringify({ store, blueberry, strawberry }) },
+        ],
+    });
+    await producer.disconnect();
+
+    res.json({ status: 'success' });
+};
